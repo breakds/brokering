e872ee491cb75cb999a0a5e8033d4d4c504658d0
Break Yang <breakds@gmail.com>
2020-07-13 22:08:26 -0700

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,20 @@
+let pkgs = import <nixpkgs> {
+      config.allowUnfree = true;
+    };
+
+    python = pkgs.python3.withPackages (python-packages: with python-packages; [
+      setuptools
+      click
+      flask
+      psutil
+      pyyaml
+      twisted
+    ]);
+
+in pkgs.mkShell rec {
+  name = "pyd";
+  buildInputs = with pkgs; [ python sqlitebrowser parted ];
+  shellHook = ''
+    export PS1="$(echo -e '\uf3e2') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
+  '';
+}
